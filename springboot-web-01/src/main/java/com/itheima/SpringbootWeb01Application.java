57a4c6031e2d7dbb4d3b1476a460d789ccc81dd5
wss <wss2023@nefu.edu.cn>
2025-05-15 00:02:07 +0800

    初次提交

--- springboot-web-01/src/main/java/com/itheima/SpringbootWeb01Application.java
+++ springboot-web-01/src/main/java/com/itheima/SpringbootWeb01Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootWeb01Application {
-
+    //主方法
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWeb01Application.class, args);
-    }
+    }  //
 
 }
